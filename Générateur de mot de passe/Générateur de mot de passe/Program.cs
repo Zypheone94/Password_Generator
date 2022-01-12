5ee474381575faec704fda5ebe7101ce0ae234f0
MaximeMohca <webmaster@mohca-communication.com>
2022-01-12 10:12:34 +0100

    creation repo git + ajout du générateur de mot de passe

---
+++ Générateur de mot de passe/Générateur de mot de passe/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FormationCS;
+
+namespace Générateur_de_mot_de_passe
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            const int NB_PASSWORD = 10;
+
+            int passwordLength = outils.AskPositivNumber("Longueur du mot de passe : ");
+            Console.WriteLine("");
+
+            int passwordType;
+
+            passwordType = outils.AskIntervalNumber("Quel type de mot de passe voulez vous ?\n" +
+                "\n" +
+                "1 - Uniquement des caractères en minuscules \n" +
+                "2 - Des caractères minuscules et majuscules \n" +
+                "3 - Des caractères et des chiffres \n" +
+                "4 - Caractères, chiffres et des caractères spéciaux\n" +
+                "Votre choix : ", 1, 4);
+
+            Console.WriteLine("");
+
+            string minuscule = "abcdefghijklmnopqrstuvwxyz";
+            string majuscule = minuscule.ToUpper();
+            string numbers = "0123456789";
+            string chara = "#!?+-@&()$";
+            string alpha;
+
+            if(passwordType == 1) { alpha = minuscule; }
+            else if(passwordType == 2) { alpha = minuscule + majuscule; }
+            else if(passwordType == 3) { alpha = minuscule + majuscule + numbers; }
+            else { alpha = minuscule + majuscule + numbers + chara; }
+
+            
+            int alphaLength = alpha.Length;
+
+            Random rand = new Random();
+
+            for (int j = 0; j < NB_PASSWORD; j++)
+            {
+                string password = "";
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    int index = rand.Next(1, alphaLength);
+                    password += alpha[index];
+                }
+                Console.WriteLine($"password : {password}");
+            }
+            
+        }
+    }
+}
---
+++ Générateur de mot de passe/Générateur de mot de passe/outils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class outils
+    {
+        public static int AskPositivNumber(string question)
+        {
+            return AskIntervalNumber(question, 1, int.MaxValue, "Erreur : La valeur ne peux être négative ou nulle");
+        }
+
+        public static int AskIntervalNumber(string question, int min, int max, string errorMessage = null)
+        {
+            int passwordLength = AskNumber(question);
+            while (true)
+            {
+                if (passwordLength >= min && passwordLength <= max) { return passwordLength; }
+
+                if (errorMessage == null) 
+                {
+                    passwordLength = AskNumber($"Incorrect, la valeur doit être contenu entre : {min} et {max}\n");
+                }
+
+                else { Console.WriteLine(errorMessage); }
+
+                return AskIntervalNumber(question, min, max);
+            }
+        }
+        public static int AskNumber(string question)
+        {
+            int passwordNumber;
+            while (true)
+            {
+                Console.Write(question);
+                string numberString = Console.ReadLine();
+                try
+                {
+                    passwordNumber = int.Parse(numberString);
+                    return passwordNumber;
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur : Veuillez rentrer une valeur valide\n");
+                }
+            }
+        }
+    }
+}
